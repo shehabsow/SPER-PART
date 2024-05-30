496dd6c4923f3b7c7aebac18a91c41fc62d5b2d2
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-05-30 21:59:17 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -888,9 +888,11 @@ if page == 'Utility area':
         st.subheader('Select from these items')
 
         if select_col == 'Water Station':
+            col1, col2, col3 = st.columns([10,3,13])
+            with col1:
             
-            tab1, tab2 ,tab3, tab4,tab5, tab6 ,tab8, tab9 ,tab10, tab12,tab13 = st.tabs(['Conductivity transmitter','Flowmeter controller','Flow module',
-            'Flow monitor','conductivity','Stilmas sensor','Valve','test','pump','Uv','Ro'])
+                tab1, tab2 ,tab3, tab4,tab5, tab6 ,tab8, tab9 ,tab10, tab12,tab13 = st.tabs(['Conductivity transmitter','Flowmeter controller','Flow module',
+                'Flow monitor','conductivity','Stilmas sensor','Valve','test','pump','Uv','Ro'])
             
             with tab1:
                 col1, col2, col3 = st.columns([30,3,13])
