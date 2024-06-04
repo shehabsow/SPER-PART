6f2d837241322117e6aba5814cfdcea177ef51bd
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-06-04 17:16:12 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -12,7 +12,7 @@ st.set_page_config(
 
 
 
-
+df_f = pd.read_csv('Eng Spare parts.csv')
 csv_path = 'Eng Spare parts.csv'
 page =  st.sidebar.radio('Select page', ['Utility area','Mechanical parts', 'Electrical parts',
                     'Pneumatic parts','GLATT','FETTE','FORKLIFT','LOTOTO'])
