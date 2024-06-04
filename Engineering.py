b72f40ac97a1161bcf4966f28d2ba558fc1b31d7
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-06-04 17:11:02 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -11,7 +11,7 @@ st.set_page_config(
 )
 
 
-url = 'https://drive.google.com/file/d/1D-616oIyvdCAudFIRJb5s1A8KGHiGfvv/view?usp=sharing'
+
 
 csv_path = 'Eng Spare parts.csv'
 page =  st.sidebar.radio('Select page', ['Utility area','Mechanical parts', 'Electrical parts',
@@ -52,6 +52,7 @@ if page == 'Mechanical parts':
         with tab1:
             col1, col2, col3 = st.columns([30,3,13])
             with col1:
+                url = 'https://drive.google.com/file/d/1D-616oIyvdCAudFIRJb5s1A8KGHiGfvv/view?usp=sharing'
                 @st.cache_data
                 def load_data():
                     # تحميل الملف من Google Drive
