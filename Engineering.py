cbe9928f533288481c3472c9cb7de9ab03b3bffc
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-06-04 18:13:21 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -58,48 +58,51 @@ if page == 'Mechanical parts':
 
                 # عرض الداتا فري
                 # اختيار رقم الصف
-                row_number = st.number_input('Select row number:', min_value=0, max_value=len(df_f)-1, step=1)
+                col11, col12, col13 = st.columns([1,2,2])
+                with col11:    
+                    row_number = st.number_input('Select row number:', min_value=0, max_value=len(df_f)-1, step=1)
 
                 # عرض المعلومات عن الصف المختار
-                st.write(f"Selected Item : {df_f.loc[row_number, 'Item description']}")
-                st.write(f"Current Quantity : {df_f.loc[row_number, 'Qty.']}")
-
-                # اختيار كمية الخصم
-                deduct_quantity = st.number_input('Enter quantity to deduct :', min_value=1, max_value=int(df_f.loc[row_number, 'Qty.']), step=1)
-
-                # زر لتحديث الكمية
-                if 'update_button_clicked' not in st.session_state:
-                    st.session_state.update_button_clicked = False
-
-# زر لتحديث الكمية
-                if st.button('Update Quantity'):
-                    if not st.session_state.update_button_clicked:
-        # خصم الكمية المحددة
-                        df_f.loc[row_number, 'Qty.'] -= deduct_quantity
-                        st.success(f'{deduct_quantity} units deducted from {df_f.loc[row_number, "Item description"]}.')
-                        
-                        # تحديث البيانات في ملف CSV
-                        df_f.to_csv(csv_path, index=False)
-                        
-                        # تحديد حالة الزر على أنه تم الضغط عليه
-                        st.session_state.update_button_clicked = True
-                        
-                        # إعادة تحميل البيانات من ملف CSV لتحديث العرض
-                        st.experimental_rerun()
-
-# إعادة تعيين حالة الزر عند إعادة تحميل الصفحة
-                if st.session_state.update_button_clicked:
-                    st.session_state.update_button_clicked = False
-
-# تحميل الملف عند الطلب
-                
-                csv = df_f.to_csv(index=False).encode('utf-8')
-                st.download_button(
-                    label="Download CSV",
-                    data=csv,
-                    file_name='updated_inventory.csv',
-                    mime='text/csv',
-                )
+                    
+                    st.write(f"Selected Item : {df_f.loc[row_number, 'Item description']}")
+                    st.write(f"Current Quantity : {df_f.loc[row_number, 'Qty.']}")
+    
+                    # اختيار كمية الخصم
+                    deduct_quantity = st.number_input('Enter quantity to deduct :', min_value=1, max_value=int(df_f.loc[row_number, 'Qty.']), step=1)
+    
+                    # زر لتحديث الكمية
+                    if 'update_button_clicked' not in st.session_state:
+                        st.session_state.update_button_clicked = False
+    
+    # زر لتحديث الكمية
+                    if st.button('Update Quantity'):
+                        if not st.session_state.update_button_clicked:
+            # خصم الكمية المحددة
+                            df_f.loc[row_number, 'Qty.'] -= deduct_quantity
+                            st.success(f'{deduct_quantity} units deducted from {df_f.loc[row_number, "Item description"]}.')
+                            
+                            # تحديث البيانات في ملف CSV
+                            df_f.to_csv(csv_path, index=False)
+                            
+                            # تحديد حالة الزر على أنه تم الضغط عليه
+                            st.session_state.update_button_clicked = True
+                            
+                            # إعادة تحميل البيانات من ملف CSV لتحديث العرض
+                            st.experimental_rerun()
+    
+    # إعادة تعيين حالة الزر عند إعادة تحميل الصفحة
+                    if st.session_state.update_button_clicked:
+                        st.session_state.update_button_clicked = False
+    
+    # تحميل الملف عند الطلب
+                    
+                    csv = df_f.to_csv(index=False).encode('utf-8')
+                    st.download_button(
+                        label="Download CSV",
+                        data=csv,
+                        file_name='updated_inventory.csv',
+                        mime='text/csv',
+                    )
 
             with col3:
                 st.subheader('image  for  these  part')
