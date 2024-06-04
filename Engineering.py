0f0223845253d70740d474873516b2ba49ed2077
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-06-04 18:20:59 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -60,7 +60,7 @@ if page == 'Mechanical parts':
                 # اختيار رقم الصف
                 col11, col12, col13 = st.columns([1,2,2])
                 with col11:    
-                    row_number = st.number_input('Select row number:', min_value=0, max_value=len(df_f)-1, step=1)
+                    row_number = st.number_input('Select row number:', min_value=0.0, max_value=len(df_f)-1, step=1)
 
                 # عرض المعلومات عن الصف المختار
                     
