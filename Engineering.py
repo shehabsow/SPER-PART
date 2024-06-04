0eab727d57ef01d36d5367e81480c813a03514da
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-06-04 20:49:08 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -158,16 +158,16 @@ if page == 'Mechanical parts':
             with col1:
                 Belts = df_f[df_f['Comments'] == 'Belts'].sort_values(by='Comments')
                 st.dataframe(Belts,width=2000)
-                col4, col5, col6= st.columns([1,2,2])
-                with col4:    
+                col10, col11, col12 st.columns([1,2,2])
+                with col10:    
                     row_number = st.number_input('Select row number:', min_value=0, max_value=len(df_f)-1, step=1)
 
                 # عرض المعلومات عن الصف المختار
                     
                 st.write(f"Selected Item : {df_f.loc[row_number, 'Item description']}")
                 st.write(f"Current Quantity : {df_f.loc[row_number, 'Qty.']}")
-                col7, col8, col9 = st.columns([1,2,2])
-                with col7:
+                col13, col14, col15 = st.columns([1,2,2])
+                with col13:
     
                     deduct_quantity = st.number_input('Enter quantity to deduct :', min_value=0, max_value=int(df_f.loc[row_number, 'Qty.']), step=1)
     
