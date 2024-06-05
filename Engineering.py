45012f68972ffbe9982ff5d1b9f8f762056d8cec
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-06-05 03:23:54 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -625,7 +625,7 @@ if page == 'Electrical parts':
                 st.write(f"Current Quantity : {df_f.loc[row_number, 'Qty.']}")
                 add_quantity = st.number_input('Enter quantity to add for Tab1:', min_value=0, step=1, key='tab1_qty')
                 if st.button('Update Quantity'):
-                    update_quantity(row_number, add_quantity
+                    update_quantity(row_number, add_quantity)
             with col3:
                 st.subheader('image  for  these  part')
 
