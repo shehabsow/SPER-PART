97b7ea8762d21e79add6b3a6a41d5be75456d79d
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-06-05 03:43:27 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -509,7 +509,7 @@ if page == 'Electrical parts':
                 
                 Proximity = df_f[df_f['Comments'] == 'Proximity'].sort_values(by='Comments')
                 st.dataframe(Proximity)    
-                row_number = st.number_input('Select row number for Tab1:', df_f.index, key='tab1_row')
+                row_number = st.number_input('Select row number:', min_value=0, max_value=len(df)-1, step=1, key='row_tab1')
                 st.write(f"Selected Item : {df_f.loc[row_number, 'Item description']}")
                 st.write(f"Current Quantity : {df_f.loc[row_number, 'Qty.']}")
                 quantity = st.number_input('Enter quantity for Tab1:', min_value=0, step=1, key='tab1_qty')
