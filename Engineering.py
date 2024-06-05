d99c409bfea13f7dc977f7b3cc197b927075eab0
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-06-05 03:55:36 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -498,7 +498,7 @@ if page == 'Electrical parts':
                 df_f.loc[row_index, 'Qty.'] -= quantity
             df_f.to_csv('data.csv', index=False)
             st.success(f"Quantity updated successfully! New Quantity: {df_f.loc[row_index, 'Qty.']}")
-            st.experimental_rerun()
+            
         
         tab1, tab2 ,tab3, tab4,tab5, tab6 ,tab7, tab8, tab9 ,tab10, tab11 ,tab12  = st.tabs(['Proximity','Sensor','Fiber sensor','Amplifier','Socket',
         'Selector','Button','Switch','Light','Fan','Cable','Fuse'])
@@ -518,6 +518,7 @@ if page == 'Electrical parts':
                 operation = st.radio('Choose operation:', ('add', 'subtract'), key='tab1_op')
                 if st.button('Update Quantity for Tab1', key='tab1_btn'):
                     update_quantity(row_number, quantity, operation)
+                    st.experimental_rerun()
 
             with col3:
                 st.subheader('image  for  these  part')
