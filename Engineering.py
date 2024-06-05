200ff6494ba3360dfb2bc7a739d72dfef030ee1b
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-06-05 18:11:54 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -508,7 +508,7 @@ if page == 'Electrical parts':
                 
                 Proximity = df_f[df_f['Comments'] == 'Proximity'].sort_values(by='Comments')
                 st.dataframe(Proximity)
-                col11, col12, col13 = st.columns([2,3,2])
+                col11, col12, col13 = st.columns([3,1,2])
                 with col11:
                 
                     row_number = st.number_input('Select row number:', min_value=0, max_value=len(df_f)-1, step=1)
