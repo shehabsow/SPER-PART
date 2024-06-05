bfac6919d5d777e22642521e9097c12b204fdea1
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-06-05 17:09:27 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -489,6 +489,11 @@ if page == 'Electrical parts':
             df_f.to_csv('data.csv', index=False)
             st.success(f"Quantity updated successfully! New Quantity: {df_f.loc[row_index, 'Qty.']}")
             st.session_state.update_button_clicked = True
+            row_number = st.number_input('Select row number:', min_value=0, max_value=len(df_f)-1, step=1)
+            st.write(f"Selected Item : {df_f.loc[row_number, 'Item description']}")
+            st.write(f"Current Quantity : {df_f.loc[row_number, 'Qty.']}")
+            quantity = st.number_input('Enter quantity for Tab1:', min_value=0, step=1)
+            operation = st.radio('Choose operation:', ('add', 'subtract'), key='tab1_op')
             
                 
                 # إعادة تحميل البيانات من ملف CSV لتحديث العرض
@@ -507,11 +512,7 @@ if page == 'Electrical parts':
                 
                 Proximity = df_f[df_f['Comments'] == 'Proximity'].sort_values(by='Comments')
                 st.dataframe(Proximity)    
-                row_number = st.number_input('Select row number:', min_value=0, max_value=len(df_f)-1, step=1)
-                st.write(f"Selected Item : {df_f.loc[row_number, 'Item description']}")
-                st.write(f"Current Quantity : {df_f.loc[row_number, 'Qty.']}")
-                quantity = st.number_input('Enter quantity for Tab1:', min_value=0, step=1)
-                operation = st.radio('Choose operation:', ('add', 'subtract'), key='tab1_op')
+                
                 if st.button('Update Quantity'):
                     update_quantity(row_number, quantity, operation)
                 
