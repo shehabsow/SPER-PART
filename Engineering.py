6f11b2356c92406b0228b3b0f9325b8d8b361295
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-06-05 20:26:13 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -18,62 +18,62 @@ page =  st.sidebar.radio('Select page', ['Utility area','Mechanical parts', 'Ele
                     'Pneumatic parts','GLATT','FETTE','FORKLIFT','LOTOTO'])
 if 'df' not in st.session_state:
             st.session_state.df = pd.read_csv('Eng Spare parts.csv')
-        df_f = st.session_state.df
-        def update_quantity(row_index, quantity, operation):
-            if operation == 'add':
-                df_f.loc[row_index, 'Qty.'] += quantity
-            elif operation == 'subtract':
-                df_f.loc[row_index, 'Qty.'] -= quantity
-            df_f.to_csv('data.csv', index=False)
-            st.success(f"Quantity updated successfully! New Quantity: {df_f.loc[row_index, 'Qty.']}")
-            st.session_state.update_button_clicked = True
+df_f = st.session_state.df
+def update_quantity(row_index, quantity, operation):
+    if operation == 'add':
+        df_f.loc[row_index, 'Qty.'] += quantity
+    elif operation == 'subtract':
+        df_f.loc[row_index, 'Qty.'] -= quantity
+    df_f.to_csv('data.csv', index=False)
+    st.success(f"Quantity updated successfully! New Quantity: {df_f.loc[row_index, 'Qty.']}")
+    st.session_state.update_button_clicked = True
 
-        def display_tab(tab_name):
-            st.header(f'{tab_name} Tab')
-        
-            # تنسيق Select row number
-            st.markdown("""
-                <style>
-                .custom-input label {
-                    font-size: 20px; 
-                    color: blue; 
-                }
-                .custom-input input {
-                    font-size: 18px;
-                    color: black;
-                    width: 10px; /* عرض مربع الإدخال */
-                }
-                </style>
-                """, unsafe_allow_html=True)
-            row_number = st.number_input(f'Select row number for {tab_name}:', min_value=0, max_value=len(df_f)-1, step=1, key=f'{tab_name}_row_number')
-        
-            # تنسيق الكتابة
-            st.markdown(f"""
-                <div style='font-size: 20px; color: green;'>Selected Item: {df_f.loc[row_number, 'Item description']}</div>
-                <div style='font-size: 20px; color: green;'>Current Quantity: {df_f.loc[row_number, 'Qty.']}</div>
-                """, unsafe_allow_html=True)
-            
-            # تنسيق Enter quantity
-            st.markdown("""
-                <style>
-                .custom-quantity-input label {
-                    font-size: 20px; 
-                    color: red; 
-                }
-                .custom-quantity-input input {
-                    font-size: 18px;
-                    color: black;
-                    width: 10px; /* عرض مربع الإدخال */
-                }
-                </style>
-                """, unsafe_allow_html=True)
-            quantity = st.number_input(f'Enter quantity for {tab_name}:', min_value=0, step=1, key=f'{tab_name}_quantity')
-        
-            operation = st.radio(f'Choose operation for {tab_name}:', ('add', 'subtract'), key=f'{tab_name}_operation')
-        
-            if st.button(f'Update Quantity for {tab_name}', key=f'{tab_name}_update_button'):
-                update_quantity(row_number, quantity, operation)
-            csv = df_f.to_csv(index=False)
+def display_tab(tab_name):
+    st.header(f'{tab_name} Tab')
+
+    # تنسيق Select row number
+    st.markdown("""
+        <style>
+        .custom-input label {
+            font-size: 20px; 
+            color: blue; 
+        }
+        .custom-input input {
+            font-size: 18px;
+            color: black;
+            width: 10px; /* عرض مربع الإدخال */
+        }
+        </style>
+        """, unsafe_allow_html=True)
+    row_number = st.number_input(f'Select row number for {tab_name}:', min_value=0, max_value=len(df_f)-1, step=1, key=f'{tab_name}_row_number')
+
+    # تنسيق الكتابة
+    st.markdown(f"""
+        <div style='font-size: 20px; color: green;'>Selected Item: {df_f.loc[row_number, 'Item description']}</div>
+        <div style='font-size: 20px; color: green;'>Current Quantity: {df_f.loc[row_number, 'Qty.']}</div>
+        """, unsafe_allow_html=True)
+    
+    # تنسيق Enter quantity
+    st.markdown("""
+        <style>
+        .custom-quantity-input label {
+            font-size: 20px; 
+            color: red; 
+        }
+        .custom-quantity-input input {
+            font-size: 18px;
+            color: black;
+            width: 10px; /* عرض مربع الإدخال */
+        }
+        </style>
+        """, unsafe_allow_html=True)
+    quantity = st.number_input(f'Enter quantity for {tab_name}:', min_value=0, step=1, key=f'{tab_name}_quantity')
+
+    operation = st.radio(f'Choose operation for {tab_name}:', ('add', 'subtract'), key=f'{tab_name}_operation')
+
+    if st.button(f'Update Quantity for {tab_name}', key=f'{tab_name}_update_button'):
+        update_quantity(row_number, quantity, operation)
+    csv = df_f.to_csv(index=False)
 
 
 
