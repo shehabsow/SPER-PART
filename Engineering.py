48770cb394a4c086421a8958ab99a46ca9ae4abd
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-06-25 14:12:16 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -54,6 +54,7 @@ def display_tab(tab_name):
     st.markdown(f"""
         <div style='font-size: 17px; color: green;'>Selected Item: {df_f.loc[row_number, 'Item description']}</div>
         <div style='font-size: 17px; color: green;'>Current Quantity: {int(df_f.loc[row_number, 'Qty.'])}</div>
+        <div style='font-size: 17px; color: green;'>Location: {df_f.loc[row_number, 'Location']}</div>
         """, unsafe_allow_html=True)
     
     # تنسيق Enter quantity
