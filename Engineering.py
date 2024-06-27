c78e9ba533cd516f2865894f037dd76e04cb42e4
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-06-27 14:51:52 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -22,6 +22,29 @@ df_f = pd.read_csv('Eng Spare parts.csv')
 csv_path = 'Eng Spare parts.csv'
 page =  st.sidebar.radio('Select page', ['Utility area','Mechanical parts', 'Electrical parts',
                     'Neumatic parts','FORKLIFT','LOTOTO'])
+
+search_option = st.sidebar.radio(
+    'Search in:',
+    ('Item Description', 'Location', 'Qty.', 'All Columns')
+)
+
+search_keyword = st.sidebar.text_input("Enter keyword to search:")
+
+# تعريف وظيفة البحث
+def search_in_dataframe(df, keyword, option):
+    if option == 'All Columns':
+        result = df[df.apply(lambda row: row.astype(str).str.contains(keyword, case=False).any(), axis=1)]
+    else:
+        result = df[df[option].astype(str).str.contains(keyword, case=False)]
+    return result
+
+# عرض النتائج بناءً على البحث
+if search_keyword:
+    search_results = search_in_dataframe(df, search_keyword, search_option)
+    st.write(f"Search results for '{search_keyword}' in {search_option}:")
+    st.write(search_results)
+
+
 if 'df' not in st.session_state:
     st.session_state.df = pd.read_csv('Eng Spare parts.csv')
 
