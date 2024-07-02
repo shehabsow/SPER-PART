437290f82eab091ed0a2817cf58ce6b5a68ff4da
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-07-02 13:36:07 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -114,7 +114,7 @@ if 'logged_in' not in st.session_state:
 
 if not st.session_state.logged_in:
     with st.form(key='login_form'):
-        col1, col2 = st.columns([1, 2])
+        col1, col2 = st.columns([.5, 1])
         with col1:
             username = st.text_input("Username")
             password = st.text_input("Password", type="password")
