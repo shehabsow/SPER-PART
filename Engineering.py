c359461bd01e0d03b92ba827aeca385ed74a9f5b
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-08-03 12:12:47 +0300

    Update Engineering.py

--- Engineering.py
+++ Engineering.py
@@ -1847,7 +1847,7 @@ else:
                         st.components.v1.html(f'<a href="{url}" target="_blank" style="background-color: #FFD700;">Go to Web Page</a>')
 
                    with tab14:
-                        col1, col2, col3 = st.columns([30,3,13])
+                       col1, col2, col3 = st.columns([30,3,13])
                     with col1:
                         TOC = df_f[df_f['Comments'] == 'TOC'].sort_values(by='Comments')
                         st.dataframe(Ro,width=2000)
